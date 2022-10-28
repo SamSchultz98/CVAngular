9fa478f0b1a551ca0bf22bc66e4f84d4bba403fb
Sam Schultz <sam.schultz@outlook.com>
2022-10-28 09:37:13 -0400

    Stage2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { EducationExperienceComponent } from './education-experience/education-experience.component';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  declarations: [
+    WorkExperienceComponent,
+    EducationExperienceComponent
+  ]
 })
 export class AppModule { }
